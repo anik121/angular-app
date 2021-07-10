2f788a5fab1667f412a4e2ffb3b96176ef8393ae
anik121 <rifatislam30@gmail.com>
2021-07-10 23:54:11 +0600

    successfully adding router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,48 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
+// Start App Custom Component
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { TaskComponent } from './component/task/task.component';
 import { FooterComponent } from './component/footer/footer.component';
 import { TaskService } from './service/task.service';
+import { SettingComponent } from './component/setting/setting.component';
+import { NoteListComponent } from './component/note-list/note-list.component';
+import { CreateTaskComponent } from './component/create-task/create-task.component';
+import { HomeComponent } from './component/home/home.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'createTask',
+    component: CreateTaskComponent,
+  },
+  {
+    path: 'noteList',
+    component: NoteListComponent,
+  },
+  {
+    path: 'setting',
+    component: SettingComponent,
+  },
+];
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, TaskComponent, FooterComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    TaskComponent,
+    FooterComponent,
+    SettingComponent,
+    NoteListComponent,
+    CreateTaskComponent,
+    HomeComponent,
+  ],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   providers: [TaskService],
   bootstrap: [AppComponent],
 })
