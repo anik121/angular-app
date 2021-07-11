8247b2bfd0f52a1de932a0bc70932a50b9cd3369
anik121 <rifatislam30@gmail.com>
2021-07-11 14:26:54 +0600

    change design idea

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
-# Angular Task App
+## Restaurant Booking Angular App
 
 This is my first angular app. I am trying to make an app with angular alongside angular education so that I can learn angular framework quickly.
 
-[Take beautifully screenshot](https://ray.so/)
-[Task App Desktop Dashboard](https://dribbble.com/shots/6992099-G-Track-App-Dashboard)
-[Task App Authorization Pages](https://dribbble.com/shots/6992062-G-Track-App-Authorization)
+[Restaurant Booking App UiKit](https://www.uplabs.com/posts/restaurant-booking-app-uikit)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,51 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
 
-// Start App Custom Component
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './component/header/header.component';
-import { TaskComponent } from './component/task/task.component';
-import { FooterComponent } from './component/footer/footer.component';
-import { TaskService } from './service/task.service';
-import { SettingComponent } from './pages/setting/setting.component';
-import { NoteListComponent } from './pages/note-list/note-list.component';
-import { CreateTaskComponent } from './pages/create-task/create-task.component';
-import { HomeComponent } from './pages/home/home.component';
-import { TaskHeaderComponent } from './pages/note-list/task-header/task-header.component';
 
-const appRoutes: Routes = [
-  {
-    path: '',
-    component: HomeComponent,
-  },
-  {
-    path: 'createTask',
-    component: CreateTaskComponent,
-  },
-  {
-    path: 'noteList',
-    component: NoteListComponent,
-  },
-  {
-    path: 'setting',
-    component: SettingComponent,
-  },
-];
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    TaskComponent,
-    FooterComponent,
-    SettingComponent,
-    NoteListComponent,
-    CreateTaskComponent,
-    HomeComponent,
-    TaskHeaderComponent,
+    AppComponent
   ],
-  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
-  providers: [TaskService],
-  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
