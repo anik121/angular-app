94e08f9542726d342db24d581721f8b7adfb65de
anik121 <rifatislam30@gmail.com>
2021-07-13 02:21:04 +0600

    booking history page done

--- src/app/pages/booking-history/booking-history.component.ts
+++ src/app/pages/booking-history/booking-history.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { SliderService } from 'src/app/service/slider.service';
+interface Hotel {
+  id: number;
+  name: string;
+  location: string;
+  price: number;
+  description: string;
+  imgPath: string;
+}
 @Component({
   selector: 'app-booking-history',
   templateUrl: './booking-history.component.html',
-  styleUrls: ['./booking-history.component.scss']
+  styleUrls: ['./booking-history.component.scss'],
 })
 export class BookingHistoryComponent implements OnInit {
-
-  constructor() { }
+  hotels: Hotel[] = [];
+  constructor(private _hotelService: SliderService) {}
 
   ngOnInit(): void {
+    this._hotelService.getHotel().subscribe((hotel) => (this.hotels = hotel));
   }
-
 }
